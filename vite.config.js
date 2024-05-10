b5c8e50d11335ef522f327e4628e99e7f9b595d4
techymohit <mj3468246@gmail.com>
2024-05-10 14:08:19 +0530

    page not found

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-
+   publicDir: './public',
    plugins: [react()],
    server: {
       proxy: {
@@ -21,3 +21,5 @@ export default defineConfig({
       },
    },
 });
+
+
